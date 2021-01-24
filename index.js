671bc4050f59d5602132ccf9205aae22cdb04b42
binhus <eng.robsonwilli@gmail.com>
2021-01-24 12:27:01 -0300

    agora vai 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const controller = require('../MONGO_EXPRESS_REACT_NODE_API/Controllers/index');
+const controller = require('./Controllers/index');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
