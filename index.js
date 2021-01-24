0c6ee5d36324a1077b1e05bd7bbd43ca78f2b7af
binhus <eng.robsonwilli@gmail.com>
2021-01-24 12:23:44 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const controller = require('../../React-Native_Mongo_Express_Node/Controllers/index');
+const controller = require('../MONGO_EXPRESS_REACT_NODE_API/Controllers/index');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
